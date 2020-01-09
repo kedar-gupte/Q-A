99e0d6c83bc930cce85db42aced288985fe14ab0
Kedar Gupte <kedar.apache@gmail.com>
2020-01-09 21:29:36 +0530

    8 January, 2020 | Remove duplicates from sorted array

---
+++ src/main/java/Base/ArraysString/RemoveDuplicates.java
@@ -0,0 +1,62 @@
+package Base.ArraysString;
+
+import Base.BaseExecutor;
+
+import java.util.Arrays;
+
+/**
+ * Remove duplicates from sorted array
+ * https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+ *
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ *
+ * Given nums = [1,1,2],
+ *
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ *
+ * It doesn't matter what you leave beyond the returned length.
+ *
+ */
+public class RemoveDuplicates implements BaseExecutor {
+
+    public int removeDuplicates(int[] nums) {
+        int prev = 0, index = 1;
+
+        if(nums == null || nums.length <= 1) {
+            return nums.length;
+        }
+
+        while(index < nums.length) {
+
+            while(index < nums.length && nums[prev] == nums[index]) {
+                index++;
+            }
+
+            prev++;
+
+            if(index < nums.length) {
+                nums[prev] = nums[index];
+            }
+
+        }
+
+        return prev;
+    }
+
+    @Override
+    public void execute() {
+
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static void main(String[] args) {
+        new RemoveDuplicates().execute();
+    }
+}
--- src/main/java/SortingSearching/MedianOfTwoSortedArrays.java
+++ src/main/java/SortingSearching/MedianOfTwoSortedArrays.java
@@ -5,6 +5,24 @@ import Base.BaseExecutor;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/**
+ * Median of two sorted Arrays
+ * https://leetcode.com/explore/interview/card/google/63/sorting-and-searching-4/3080/
+ *
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ *
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ *
+ * You may assume nums1 and nums2 cannot be both empty.
+ *
+ * Example 1:
+ *
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ *
+ * The median is 2.0
+ *
+ */
 public class MedianOfTwoSortedArrays implements BaseExecutor {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
