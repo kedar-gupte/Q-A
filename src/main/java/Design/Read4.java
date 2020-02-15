015aafed27c23ef954afb8b1fbaa101fd61a1a9e
Kedar Gupte <kedar.apache@gmail.com>
2020-02-15 14:54:56 +0530

    25 January, 2020 | Read from buffer with cursor

---
+++ src/main/java/Design/Read4.java
@@ -0,0 +1,37 @@
+package Design;
+
+public class Read4 {
+
+    int i = 0, size = 0, index = 0;
+    char[] readBuffer = new char[4];
+
+    public int read(char[] buf, int n) {
+
+        i = 0;
+
+        while(i < n) {
+
+            if(size == 0) {
+                size = read4(readBuffer);
+                index = 0;
+                if(size == 0)
+                    break;
+            } else {
+                buf[i] = readBuffer[index];
+                index++;
+                i++;
+                size--;
+            }
+
+
+        }
+
+        return i;
+
+    }
+    
+    Integer read4(char[] buffer) {
+        return null;
+    }
+
+}
